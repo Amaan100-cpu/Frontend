800008af2139552c615f2cc051c61776a2a3f613
Amaan100-cpu <amaanahmad8616@gmail.com>
2025-09-19 11:56:30 +0530

    Update AdminOrders.jsx

--- src/components/AdminOrders.jsx
+++ src/components/AdminOrders.jsx
@@ -65,7 +65,7 @@ catch(err){
             {
               order.orderDetails.map((obj)=>{
                 return<div className='order-card2'>
-                  <p className='cartImgContainer'><img src={`${obj?.img}`} /></p>
+                  <p className='cartImgContainer order-card9'><img src={`${obj?.img}`} /></p>
                   <div>
                   <p><strong>{obj.productName}</strong></p>
                   <p style={{display:"flex",gap:"20px",flexWrap:"wrap"}}><spam>Price : {obj.price}</spam> <spam>Quantity : {obj.productQuantity}</spam> <spam>Size : {obj.size}</spam></p>
